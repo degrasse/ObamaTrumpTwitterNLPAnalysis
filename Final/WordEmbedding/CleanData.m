c6ea65cb79222ece483e74451eaf7d2f24ae3ca6
deGrasse <32888721+degrasse@users.noreply.github.com>
2019-06-16 11:36:01 -0400

    Update CleanData.m

--- Final/WordEmbedding/CleanData.m
+++ Final/WordEmbedding/CleanData.m
@@ -4,21 +4,17 @@ load 'obama_clean_word_list.mat'
 newObama= word_list(~cellfun('isempty',word_list));
 newTrump = trumptweets(~cellfun('isempty',trumptweets));
  
-
-
 obamaTokens = tokenize(newObama);
 newTokensO =obamaTokens(~cellfun('isempty',obamaTokens));
 
-
- countO = CountTokens(newTokensO);
+countO = CountTokens(newTokensO);
 OneDimObama = FixCells(newTokensO,countO);
-
 
 trumpTokens = tokenize(newTrump);
 
 newTokens =trumpTokens(~cellfun('isempty',trumpTokens));
 
- countT = CountTokens(newTokens);
+countT = CountTokens(newTokens);
 OneDimTrump = FixCells(newTokens,countT);
 
 save finalData.mat
